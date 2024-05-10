14deb3acfa71e95eebbab7fe5acc1e7165ed885c
Willy Batista <167257461+Willy9215@users.noreply.github.com>
2024-05-10 19:56:40 -0300

    hj

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement; //posso comentar suave colocando barra barra
+/* */
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function changeTheme () {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    
+}
