aaaf071a5bc10ff165e5e27af78cf7c91eadbd1a
Willy Batista <167257461+Willy9215@users.noreply.github.com>
2024-05-10 21:09:36 -0300

    upup

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@ const rootHtml = document.documentElement; //posso comentar suave colocando barr
 const accordionHeaders = document.querySelectorAll(".accordion-header");
 const menuLinks = document.querySelectorAll(".menu-link");
 
-function changeTheme () {
+function changeTheme () {  //alternar tema dark para light
     const currentTheme = rootHtml.getAttribute("data-theme");
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
 }
+
+toggleTheme.addEventListener("click", changeTheme);
+
