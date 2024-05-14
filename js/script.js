e86f4e9cca8c76fa2e91cfa0f22e70f913a14d9b
Willy Batista <167257461+Willy9215@users.noreply.github.com>
2024-05-14 19:58:09 -0300

    up

--- js/script.js
+++ js/script.js
@@ -15,4 +15,20 @@ function changeTheme () {  //alternar tema dark para light
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+//fim função mudar tema do site
 
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+        //fim da função accordion
+    })
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
